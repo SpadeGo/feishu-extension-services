b15f7151fc52bf3d499e543e9cc9e1c4e8590d2f
李远锋 <1163111845@qq.com>
2026-07-06 21:02:25 +0800

    fix: widen global CORS Access-Control-Allow-Headers to *

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -51,7 +51,7 @@ func corsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
 		if r.Method == "OPTIONS" {
 			w.WriteHeader(204)
 			return
